a9c3420ee49f5aba3d1bce29278249813a19f026
Monish Madineni <monish.madineni@gmail.com>
2025-10-01 07:51:09 +0530

    dynamically use college types

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
+
+type=['GOVT','PVT','UNIV']
+
 @app.route("/")
-def hello_world():
-    return "hrllo,world"
+def hello():
+    return render_template('browse1.html',TYPE=type)
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True)
