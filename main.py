211db35c5a2d0137473c1a4bfb0ea0cda832e766
Monish Madineni <monish.madineni@gmail.com>
2026-03-18 11:26:38 +0530

    update before merge

--- main.py
+++ main.py
@@ -67,10 +67,11 @@ engine = create_engine(
     connect_args={"check_same_thread": False}
 )
 
-
+SQLModel.metadata.create_all(engine)
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
